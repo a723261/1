8cbf75d88da9fd16a23d72e9b8b635d05a91a3cf
a723261 <a723261@gmail.com>
2024-11-18 12:18:26 +0000

    1

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask("__name__")
 def index():
     return(render_template("index.html"))
 
+@app.route("/",methods=["GET","POST"])
+def index():
+    name = request.form.get("q")
+    return(render_template("main.html"))
+
 if __name__ == "__main__":
     app.run()
 
